47ac6946459a1f7d253cbac0711685f8f3ba3a24
peymaaaaan777 <peymankardan8@gmail.com>
2026-08-24 20:05:59 +0330

    Ad telegram bot

--- Bot.py
+++ Bot.py
@@ -1,18 +1,17 @@
 import os
 import telebot
 
-TOKEN = os.environ["BOT_TOKEN"]
+TOKEN = os.environ["8906090956:AAFPRk1NIZWhtTMmKo_bfUgzsiYuFEd5L7M"]
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(
-        message,
-        "سلام داداش 🤖🔥\nربات با موفقیت فعاله!\n\nفعلاً حالت آزمایشی است."
-    )
+    bot.reply_to(message, "سلام 👋 ربات روشنه 🚀")
 
 @bot.message_handler(func=lambda message: True)
 def reply(message):
-    bot.reply_to(message, "پیامت دریافت شد ✅")
+    bot.reply_to(message, "پیامت رو گرفتم ✅")
 
+print("Bot is running...")
 bot.infinity_polling()
