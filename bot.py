795ad20042d25ff001cb7df67043a1b223fac821
peymaaaaan777 <peymankardan8@gmail.com>
2026-08-27 03:10:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
-print("ربات آماده است 🚀")
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN پیدا نشد")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "🤖 ربات فعاله!")
+
+@bot.message_handler(commands=["status"])
+def status(message):
+    bot.reply_to(message, "🟢 ربات آنلاین است.")
+
+print("🤖 Bot is running...")
+
+bot.infinity_polling()
